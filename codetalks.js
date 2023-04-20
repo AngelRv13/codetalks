9757512bc92dcfe9abf1f5fbab12c076f351b4b5
Angel Sanchez <angelsanchez@Administrators-MacBook-Pro.local>
2023-04-20 16:09:20 -0400

    added code

---
+++ codetalks.js
@@ -0,0 +1,7 @@
+let y = 0;//variable
+let x = 0;//variable
+while (y < 4) {//while loop will run aslong as the value of y is less than 4
+  y++;//"y" is incremented by 1
+  x += y;//"x" will be incremented by each of those values respectfuly
+}
+console.log(x);//at fhe end of the loop x will be the sum of 1+2+3+4=10
